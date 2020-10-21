ebf595a877159d5dfd60076da347a6b500d53bdc
PROEM2208 <73163704+PROEM2208@users.noreply.github.com>
2020-10-21 17:18:07 +0200

    Add files via upload

---
+++ services-notaire-paris-17.php
@@ -0,0 +1,236 @@
+<html>
+<head>
+
+ 		  <title>Notaire Paris 17 Maître Elisabeth GRAUZAM immobilier famille entreprise</title>		
+		  <meta name="keywords" content="Notaire Paris 17 achat vente immobilière famille entreprise ">
+		    <meta name="description" content="Maître Elisabeth GRAUZAM, votre Notaires à Paris 17, vous accompagne 
+		  pour vos projet d'acquisitions immobilières, famille ou entreprise.">	
+ <meta name="author" content="Notaire Paris 17">
+  <meta http-equiv="content-language" content="fr" />
+		  <meta content="width=device-width, initial-scale=1" name="viewport" />
+  <meta charset="UTF-8">
+  
+<link rel="stylesheet" media="screen" type="text/css" href="style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+
+
+<link rel="icon" type="image/png" href="images/favico.PNG" /> 
+
+  <link rel="stylesheet" type="text/css" href="purecookie.css" async />
+   <link rel="canonical" href="https://grauzam-paris.notaires.fr/services-notaire-paris-17">
+  <script src="purecookie.js" async></script>
+  
+<!-- Balisage JSON-LD généré par l'outil d'aide au balisage de données structurées de Google -->
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Article",
+  "name" : "Les services de votre étude notariale",
+  "articleSection" : "Les services de votre étude notariale à Paris 17",
+  "articleBody" : "Contrats de mariage / PACS / Changements de régimes matrimoniaux</H4>\n<H4>\tDonations / Donations-partages / Anticipation successorale</H4>\n<H4>\tSuccessions</H4>\n<H4>\tSuccessions internationales</H4>\n<H4>\tGestion patrimoniale</H4>\n<H4>\tPartages et liquidations de régimes matrimoniaux (divorces)</H4>\n<H4>\tImmobilier des particuliers : acquisitions et ventes de résidences principales, secondaires et investissements locatifs,</H4>\n<H4>\tVentes immobilières de locaux commerciaux, professionnels et industriels</H4>\n<H4>\tPromotions immobilières, terrains à bâtir, division de terrains, immobiliers d’entreprise et institutionnel</H4>\n<H4>\tActes de droit des sociétés : constitutions de SCI, cessions de parts sociales, opérations sur capital, </H4>\n<H4>\tCessions de fonds de commerce</H4>\n<H4>\tConseils et assistance dans tous les domaines de l’activité notariale"
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Article",
+  "name" : " Utilisation des dernières technologies",
+  "articleSection" : "Votre étude de Maître Elisabeth GRAUZAM est équipée des <b>dernières technologies",
+  "articleBody" : "Signature électronique Visio-conference"
+}
+</script>
+  
+</head>
+	
+    <body style="font-family:Comfortaa, cursive">
+	<div class='suprmobile'>
+<center>
+<img style="max-width:100%;"  src="images/notaire-paris-17-img.jpg"   align='center'  height='346px' 
+				alt="Votre Notaire est situé à Paris 17 pres du metro pereire de son boulevard
+				 et du metro wagram. Il est aussi à votre disposition pour des signatures par visio et des actes avec la signature electronique."/> 
+            </center>
+</div>
+     <?php
+
+include('menu.php');
+?>
+	
+	
+<div class='suprmobile'>
+<br>	
+<table style=" width: 80%;padding: 0; margin:0;" cellpadding=0 cellspacing=0>
+  <tr style=" padding: 0; margin:0;" cellpadding=0 cellspacing=0>
+   <td style=" padding: 0; margin:0;" cellpadding=0 cellspacing=0></td><td width='40%' valign='center' style="font-size: 12px; align: right;padding: 0; margin:0; border-width:1px; border-style:solid; border-color:black;">
+   <b><a href='services-notaire-paris-17#notaire-paris-17' id='menulien'>  Les services de votre étude notariale </a></li><br>
+		  &nbsp;&nbsp;&nbsp;<a href='services-notaire-paris-17#immobilier-notaire-paris-17' id='menulien'>   Immobilier</a>
+		   		   <br>&nbsp;&nbsp;&nbsp;<a href='services-notaire-paris-17#famille-notaire-paris-17' id='menulien'>   Famille</a>
+				   		   <br>&nbsp;&nbsp;&nbsp;<a href='services-notaire-paris-17#professionnel-notaire-paris-17' id='menulien'>   Entreprise et professionnel</a>
+		  
+		  <br><a href='services-notaire-paris-17#notaire-technologie' id='menulien'>   Utilisation des dernières technologies</a></li>
+		  </b> 
+   </td>
+  </tr>
+</table>
+<br>
+</div>
+
+<div class='divtextemaj'>
+
+<div class='suprmobile'>
+
+<table style="width:100%;" valign="middle" >
+
+<tr>
+<td colspan='3'><h1>
+<a href='services-notaire-paris-17' style='text-decoration: none;color:black;' class='menulien'>Nos domaines d'expertises</h1>
+</td>
+</tr>
+
+
+<tr  align="center"  valign="middle"  style=" border: thin solid #6495ed;padding: 0px;margin:0; ">
+
+<td     style="color:white;background-color: #4f7cb3;width:33%;padding: 15px;margin:0; "  >
+<h2><a href='services-notaire-paris-17#immobilier-notaire-paris-17' style='text-decoration: none;color:white;'
+class='menulien'>IMMOBILIER</a></h2></td>
+
+<td       style="color:white;background-color: #4f7cb3;width:33%; padding: 15px;margin:0; "  >
+<h2><a href='services-notaire-paris-17#famille-notaire-paris-17' style='text-decoration: none;color:white;'
+class='menulien'>FAMILLE</h2></td>	
+
+<td       style="color:white;background-color: #4f7cb3;width:33%;padding: 15px;margin:0; "  >
+<h2><a href='services-notaire-paris-17#professionnel-notaire-paris-17' style='text-decoration: none;color:white;'
+class='menulien'>PROFESSIONNEL </h2></td>
+</tr>
+
+
+</table>
+</div>
+
+
+
+
+
+<a id="immobilier-notaire-paris-17"></a>
+<br>
+
+
+
+<h1><a href='notaire-paris-17-immobilier' id='menulien'>Vous avez un projet immobilier</a></h1>
+<br>
+Pour vos <b>acquisitions immobilières</b>, pour une résidence principale, 
+							secondaire ou encore pour vos investissements locatifs, Maître Elisabeth GRAUZAM, Notaire à Paris 17ème, 
+							vous conseille pour : 
+							<br>&nbsp;&nbsp; - L'offre d'achat,
+							<br>&nbsp;&nbsp; - Rédaction de la promesse de vente,
+							<br>&nbsp;&nbsp; - Obtention de crédit,
+							<br>&nbsp;&nbsp; - Rédaction de l'acte de vente. 
+							<br><br>
+Vous pouvez <a href='notaire-paris-17-prix-immobilier'> suivre <b>les prix de l'immobilier</b> grâce au baromètre des Notaires de France</a>						
+
+							<br><br>
+							Votre Notaire à Paris (17ème arrondissement) analyse aussi :
+							<br>&nbsp;&nbsp; - La fiscalité 
+							<br>&nbsp;&nbsp; - La plus-value-immobilière.
+							 <br>	 <br>	 <br>
+L'office Notarial établit les actes de prêts dans le cadre d’emprunts immobiliers.
+							 <br>		 
+
+<br><br><a href='contact.php'  class='trouvervotre'  >&nbsp;&nbsp;&nbsp;&nbsp;Pour toutes questions, contactez nous&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<br><br>
+
+<a id="famille-notaire-paris-17"></a>
+<br>
+<h1><a href='notaire-paris-17-famille' id='menulien'>Famille, Donations, Successions, Régime matrimonial</a></h1>
+<br>
+Pour vos besoins de <b>transmission de patrimoine</b> Maître Elisabeth GRAUZAM, Notaire à Paris (17ème arrondissement),  vous accompagne pour :
+						<br>&nbsp;&nbsp; -  Donations,
+						<br>&nbsp;&nbsp; -  Donations-partages,
+						<br>&nbsp;&nbsp; -  Testaments,
+						<br>&nbsp;&nbsp; -  Donations entre époux communément appelé "donation au dernier vivant"),
+						<br>&nbsp;&nbsp; -  L'optimisation fiscale selon votre besoin et votre situation. 	
+
+<br><br>
+Vous pouvez <a href='notaire-paris-17-frais-de-succession'> Calculer ici vos <b>frais de succession</b></a>						
+<br><br>
+Nous vous conseillerons aussi pour : 
+<br>&nbsp;&nbsp; -  Le régime matrimonial à adopter,
+<br>&nbsp;&nbsp; -  Le <b>contrat de mariage</b> à choisir,
+<br>&nbsp;&nbsp; -  Le changement de régime matrimonial,
+<br>&nbsp;&nbsp; -  Liquidation et partage du patrimoine commun dans le cadre de divorce.
+
+<br><br><a href='contact.php'  class='trouvervotre'  >&nbsp;&nbsp;&nbsp;&nbsp;Pour toutes questions, contactez nous&nbsp;&nbsp;&nbsp;&nbsp;</a>
+
+<a id="professionnel-notaire-paris-17"></a>
+<br><br>
+<h1><a href='notaire-paris-17-entreprise' id='menulien'>Vous êtes un professionnel</a></h1>
+<br>
+
+Maître Elisabeth GRAUZAM, vous conseille en vue de la protection de l'entrepreneur et de son entreprise :
+<br>&nbsp;&nbsp; -  Déclaration d'insaisissabilité du patrimoine privé;
+<br>&nbsp;&nbsp; -  Détermination du type de société à créer ;
+<br>&nbsp;&nbsp; -  Rédaction des statuts ;
+<br>&nbsp;&nbsp; -  Actes de cessions de parts et de fonds de commerce ;
+<br>&nbsp;&nbsp; -  Régime matrimonial du chef d’entreprise.
+
+<br><br>
+
+<br><br><a href='contact.php'  class='trouvervotre'  >&nbsp;&nbsp;&nbsp;&nbsp;Pour toutes questions, contactez nous&nbsp;&nbsp;&nbsp;&nbsp;</a>
+
+
+
+
+	<a id="notaire-paris-17"></a>
+<h1>Les services de votre étude notariale à Paris 17</h1>
+<br>
+
+
+Notre équipe vous accompagne dans <b>tous vos projets</b> : <br> <br>
+
+
+
+
+<h4 >	Contrats de mariage / PACS / Changements de régimes matrimoniaux</h4>
+<h4 >	Donations / Donations-partages / Anticipation successorale</h4>
+<h4 >	Successions</h4>
+<h4 >	Successions internationales</h4>
+<h4 >	Gestion patrimoniale</h4>
+<h4 >	Partages et liquidations de régimes matrimoniaux (divorces)</h4>
+<h4 >	Immobilier des particuliers : acquisitions et ventes de résidences principales, secondaires et investissements locatifs,</h4>
+<h4 >	Ventes immobilières de locaux commerciaux, professionnels et industriels</h4>
+<h4 >	Promotions immobilières, terrains à bâtir, division de terrains, immobiliers d’entreprise et institutionnel</h4>
+<h4 >	Actes de droit des sociétés : constitutions de SCI, cessions de parts sociales, opérations sur capital, </h4>
+<h4 >	Cessions de fonds de commerce</h4>
+<h4 >	Conseils et assistance dans tous les domaines de l’activité notariale</h4>
+
+
+
+
+
+
+
+
+
+<br><a id="notaire-technologie"></a><h1>
+L'etude de Maître Elisabeth GRAUZAM est équipée des <b>dernières technologies</b> :</h1> 
+
+
+
+<h4 >	Signature électronique </h4>
+<h4 > <img src='images/visio.PNG' alt="Maître GRAUZAM notaire Paris 17 est équipé de la Visio-conference. 
+Elle peut donc faire signer les actes à distance.">	 Visio-conference </h4>
+
+
+</div>
+
+
+
+
+</div>
+
+<?php
+
+include('footer.php');
+?>
+    </body>
+</html>
